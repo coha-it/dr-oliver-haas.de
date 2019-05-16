d8b71f4439f3add8884a02cdf39c85fc458d724e
Alexander Bachschmid <a.bachschmid@corporate-happiness.de>
2019-05-16 17:39:55 +0200

    Custom JS and CSS implemented in Child-Theme

--- wp-content/themes/dr-oliver-haas/functions.php
+++ wp-content/themes/dr-oliver-haas/functions.php
@@ -14,3 +14,18 @@ function theme_enqueue_styles() {
 //
 // Your code goes below
 //
+
+// Add Custom CSS and JS
+function add_theme_codes() {
+    // **** CSS **** //
+    // Owl CSS
+    wp_enqueue_style( 'style', get_stylesheet_directory_uri().'/src/css/owl.carousel.min.css', 'all');
+
+    // **** JavaScript **** //
+    // Owl JS
+    wp_enqueue_script( 'custom-script-1', get_stylesheet_directory_uri().'/src/js/owl.carousel.min.js', ['jquery']);
+    wp_enqueue_script( 'custom-script-2', get_stylesheet_directory_uri().'/src/js/custom.js', 'all');
+
+}
+
+add_action( 'wp_enqueue_scripts', 'add_theme_codes' );
