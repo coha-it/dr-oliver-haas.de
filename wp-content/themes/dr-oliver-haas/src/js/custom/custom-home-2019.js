bb57d650d638016a363813c6f5b696f11d7400b3
Alexander Bachschmid <a.bachschmid@corporate-happiness.de>
2019-05-21 21:48:12 +0200

    CustomJS und CSS Anpassungen

--- wp-content/themes/dr-oliver-haas/src/js/custom/custom-home-2019.js
+++ wp-content/themes/dr-oliver-haas/src/js/custom/custom-home-2019.js
@@ -19,23 +19,36 @@ jQuery(document).ready(function($) {
                         .addTo(controller);
     });
 
-
-
     // JQUERY TEXT OVERFLOW
     jQuery('.textOverflow').addClass('cutted');
-    jQuery('.textOverflow .et_pb_text_inner p').append('&nbsp;&nbsp;<span class="overflowTrigger opener">[Mehr erfahren]</span><span class="overflowTrigger closer">[Weniger anzeigen]</span>');
+    jQuery('.textOverflow .et_pb_text_inner').append('&nbsp;&nbsp;<span class="overflowTrigger opener">[Mehr erfahren]</span><span class="overflowTrigger closer">[Weniger anzeigen]</span>');
 
-    // ON Overflow Trigger Click
-    jQuery('.overflowTrigger').on('click', function() {
+    // Open the Box
+    $(document).on('click', '.textOverflow.cutted .et_pb_text_inner', function() {
         var elem = $(this);
         var tof = elem.closest('.textOverflow');
 
+        console.log('open the box');
+
         // is opener
-        if(elem.hasClass('opener')) {
+        if(tof.hasClass('cutted')) {
             tof.removeClass('cutted');
-        } else if(elem.hasClass('closer')) {
-            tof.addClass('cutted');
         }
     });
+
+    // ON Overflow Trigger Click on Closer
+    $(document).on('click', '.overflowTrigger.closer', function() {
+        var elem = $(this);
+        var tof = elem.closest('.textOverflow');
+
+        console.log('close the box');
+
+        console.log('test');
+        console.log(elem);
+        console.log(elem.hasClass('closer'));
+
+        tof.addClass('cutted');
+    });
+
 
 });
