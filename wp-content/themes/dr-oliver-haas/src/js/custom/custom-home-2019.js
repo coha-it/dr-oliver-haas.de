8dda38efda566e02bc864044a78680a981587062
Alexander Bachschmid <a.bachschmid@corporate-happiness.de>
2019-05-21 12:54:38 +0200

    Styling

--- wp-content/themes/dr-oliver-haas/src/js/custom/custom-home-2019.js
+++ wp-content/themes/dr-oliver-haas/src/js/custom/custom-home-2019.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
-    // SCROLLMAGIC 
 
+    // SCROLLMAGIC 
     jQuery(".scrollMagicFadeInLeft > div").each(function() {
         var curr = this;
 
@@ -17,6 +17,25 @@ jQuery(document).ready(function($) {
         var scene0 = new ScrollMagic.Scene({triggerElement: curr, duration: '35%', triggerHook: 0.95})
                         .setTween(tween0)
                         .addTo(controller);
+    });
+
+
+
+    // JQUERY TEXT OVERFLOW
+    jQuery('.textOverflow').addClass('cutted');
+    jQuery('.textOverflow .et_pb_text_inner p').append('&nbsp;&nbsp;<span class="overflowTrigger opener">[Mehr erfahren]</span><span class="overflowTrigger closer">[Weniger anzeigen]</span>');
+
+    // ON Overflow Trigger Click
+    jQuery('.overflowTrigger').on('click', function() {
+        var elem = $(this);
+        var tof = elem.closest('.textOverflow');
+
+        // is opener
+        if(elem.hasClass('opener')) {
+            tof.removeClass('cutted');
+        } else if(elem.hasClass('closer')) {
+            tof.addClass('cutted');
+        }
     });
 
 });
